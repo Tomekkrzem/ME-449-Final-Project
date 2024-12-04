3b4d70c30ff2e6bfc60cf61e399df6e316785606
Tomasz Krzeminski <tomekkrzeminski12@gmail.com>
2024-12-04 13:09:11 -0600

    Commented Code

--- test/NextState_Test.py
+++ test/NextState_Test.py
@@ -2,23 +2,33 @@ from src.NextState import NextState
 import numpy as np
 import pandas as pd
 
+# Initial Configuration
 init_conf = np.array([0,0,0,0,0,0,0,0,0,0,0,0])
+# Constant Wheel Speeds and Joint Speeds
 const_control1 = np.array([-10,10,-10,10,0,0,0,0,0])
+# Time Step
 dt = 0.01
-time = 10
+# Time to Run for in Seconds
+time = 1
+# Overall Run Time of Simulation
 sim_time = time / dt
+# Gripper State
 g_state = 0
 
+# List of Configurations
 state = [init_conf]
+# List of Configurations Including Gripper State
 trajectory = [[*state[0].tolist(),g_state]]
-
+# Iteration Number
 i = 1
 
+# Loop Until Runtime is Done
 for i in range(int(sim_time)-1):
-
+    # Update Configuration Lists
     state.append(NextState(state[i-1],const_control1,dt,5))
     trajectory.append([*state[i], g_state])
     i = i + 1
+
 
 # Code Used to Generate CSV Files
 df = pd.DataFrame(trajectory)
--- test/TrajectoryGenerator_Test.py
+++ test/TrajectoryGenerator_Test.py
@@ -2,46 +2,55 @@ from src.TrajectoryGenerator import TrajectoryGenerator
 import numpy as np
 import pandas as pd
 
+# Body Frame in Space Frame
 Tsb = np.array([[1,0,0,0],
                 [0,1,0,0],
                 [0,0,1,0.0963],
                 [0,0,0,1]])
 
+# Arm Base Frame in Body Frame
 Tb0 = np.array([[1,0,0,0.1662],
                 [0,1,0,0],
                 [0,0,1,0.0026],
                 [0,0,0,1]])
 
+# End Effector Home Configuration in Arm Base Frame
 M0e = np.array([[1,0,0,0.033],
                 [0,1,0,0],
                 [0,0,1,0.6546],
                 [0,0,0,1]])
 
+# End Effector Home Configuration in Space Frame
 Mse =  np.matmul(Tsb,np.matmul(Tb0,M0e))
 
-# Given Cube Test
+# Initial Cube Configuration
 Tsc_i = np.array([[1,0,0,1],
                   [0,1,0,0],
                   [0,0,1,0.025],
                   [0,0,0,1]])
 
+# Final Cube Configuration
 Tsc_f = np.array([[0,1,0,0],
                   [-1,0,0,-1],
                   [0,0,1,0.025],
                   [0,0,0,1]])
 
+# End Effector Angle
 ee_ang = np.sin(np.pi/4)
 
+# Configuration of End Effector When Grasping
 T_grasp = np.array([[-ee_ang,0,ee_ang,0],
                     [0,1,0,0],
                     [-ee_ang,0,-ee_ang,0],
                     [0,0,0,1]])
 
+# Configuration of End Effector at Standoff
 T_standoff = np.array([[-ee_ang,0,ee_ang,0],
                        [0,1,0,0],
                        [-ee_ang,0,-ee_ang,0.15],
                        [0,0,0,1]])
 
+# Complete Trajectory Generated Using TrajectoryGenerator
 t = TrajectoryGenerator(Mse,Tsc_i,Tsc_f,T_grasp,T_standoff,10)
 
 # Code Used to Generate CSV Files
